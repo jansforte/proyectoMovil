2cf5ae508ce31f8fee45ce853c09731ab18cfa3a
jansforte <johan.fuentes01@uceva.edu.co>
2021-06-15 13:12:12 -0500

    Signed-off-by: jansforte <johan.fuentes01@uceva.edu.co>

--- ScriptsPHP/Registro_INSERT.php
+++ ScriptsPHP/Registro_INSERT.php
@@ -3,7 +3,7 @@
 
 	if($_SERVER['REQUEST_METHOD']=='POST'){
 		$datos = json_decode(file_get_contents("php://input"),true);
-		$respuesta = Registro::InsertarNuevoDato($datos["id"],$datos["tel"],$datos["password"],$datos["nombre"],$datos["direccion"],$datos["ciudad"]);
+		$respuesta = Registro::InsertarNuevoDato($datos["id"],$datos["id"],$datos["password"],$datos["nombre"],$datos["direccion"],$datos["ciudad"]);
 		$r2 = Registro::InsertarEnTablaLogin($datos["id"],$datos["password"]);
 		$r3 = Registro::CreateTable($datos["id"]);
 		if($respuesta && $r2 && $r3=200){
