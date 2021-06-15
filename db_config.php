d943971fdaea4a85bb957efd4d22ccefa67a4b6a
jansforte <johan.fuentes01@uceva.edu.co>
2021-06-15 11:48:49 -0500

    Signed-off-by: jansforte <johan.fuentes01@uceva.edu.co>

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "root"); // Usuario
+define('DB_PASSWORD', ""); // Password
+define('DB_DATABASE', "trabajo"); // Nombre de la base de datos
+define('DB_SERVER', "127.0.0.1"); // db server
+?>
